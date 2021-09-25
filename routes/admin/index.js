b0e661862ce966700bd77fe9f406d0d4fd734ae2
huangyan321 <1604549268@qq.com>
2021-09-25 22:05:51 +0800

    测试添加二级路由

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var categories = require('./categories/index');
+router.use('/categories', categories)
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send("<h1>我是一级admin路由</h1>");
 });
 
 module.exports = router;
