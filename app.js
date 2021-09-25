c4332e82ed8330b1b77c0963c324caba925c2cbb
huangyan321 <1604549268@qq.com>
2021-09-25 22:52:12 +0800

    允许跨域，fix body解析

--- app.js
+++ app.js
@@ -7,9 +7,21 @@ require('./db/db')
 var adminRouter = require('./routes/admin/index');
 
 var app = express();
-//路由定义
-app.use('/admin', adminRouter);
-app.use(require('cors'))
+
+//跨域配置
+app.all("*", function (req, res, next) {
+  //设置允许跨域的域名，*代表允许任意域名跨域
+
+  res.header("Access-Control-Allow-Origin", "*");
+  //允许的header类型
+  res.header("Access-Control-Allow-Headers", "Content-Type,Access-Token,Appid,Secret,Authorization");
+  //跨域允许的请求方式 
+  res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+  if (req.method.toLowerCase() == 'options')
+    res.sendStatus(200); //让options尝试请求快速结束
+  else
+    next();
+})
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
@@ -17,18 +29,24 @@ app.use(require('cors'))
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//定义路由 一定要在解析body-parser中间件之后引入
+//路由定义
+app.use('/admin', adminRouter)
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
