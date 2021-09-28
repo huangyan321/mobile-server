ca8defb4442c6756888ba165485c2480f9e1122b
huangyan321 <1604549268@qq.com>
2021-09-28 14:10:09 +0800

    中间件封装

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+module.exports = (key) => {
+  const modelAdmin = require("../model/Admin");
+  var jwt = require('jsonwebtoken');
+  var assert = require('http-assert')
+  return async (req, res, next) => {
+    console.log(key);
+
+    var token = req.headers.authorization;
+    assert(token, 400, "token不存在，请重新登录！");
+    var {
+      _id
+    } = await jwt.verify(token, key);
+    assert(_id, 422, "token异常，请重新登录！");
+    var user = await modelAdmin.findById(_id);
+    assert(user, 423, "用户不存在，请重新登录！");
+    next()
+  }
+}
